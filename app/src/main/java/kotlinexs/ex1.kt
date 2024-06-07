5ae633843a0cbea4b73472a930cd070926ea451b
naniankala <earnest523247@gmail.com>
2024-06-07 10:48:06 +0530

    converted file java to kotlin

---
+++ app/src/main/java/kotlinexs/ex1.kt
@@ -0,0 +1,12 @@
+package kotlinexs
+
+
+fun main() {
+    println("hello world");
+    var myStudent: Student = Student("abdul",123,"bangalore")
+    println(myStudent.name)
+    myStudent.name = "ansari"
+    println(myStudent.name)
+
+
+}
